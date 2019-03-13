69c2268430199bf814011a0ab1ae721237b08bab
vPolyovyj <vpolyovyj@gmail.com>
2019-03-13 22:57:02 +0200

    fixed path to app views

--- Labs/L2/Example/server.js
+++ Labs/L2/Example/server.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 // встановлюємо express
 const express = require('express')
 const app = express()
@@ -5,8 +7,8 @@ const app = express()
 // встановлюємо директорію для віддачі статичного контенту (каталог проекту)
 app.use(express.static(__dirname))
 
-// налаштовуємо роботу із шалонізаотором
-app.set('views', './static/views')
+// налаштовуємо роботу із шаблонізаотором
+app.set('views', path.join(__dirname, '/static/views'))
 app.set('view engine', 'pug')
 
 // налаштовуємо маршрутизацію
